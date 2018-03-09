f8397aaa0906dc26e23811b290ec369ff2960979
Sophie Kirsanova <annie.apple.sun@gmail.com>
2018-03-09 22:34:30 +0300

    Task - get the sum of inserted numbers

---
+++ task3 - done.js
@@ -0,0 +1,31 @@
+/*
+Вопрос 3
+Напишите функцию sum, вызываемую следующим образом:
+sum(1)(2)(3)....(n)(); // 1 + 2 + 3 + ... + n
+sum(1)(10)(); // 11
+sum(1)(); // 1
+*/
+
+function sum(a){
+  var result = a;
+  function add_then_repeat(b){
+    if(b){
+      result += b;
+      return add_then_repeat;
+    } else {
+      return result;
+    };
+  };
+  add_then_repeat.toString = function(){
+      return result;
+    };
+    return add_then_repeat;
+  };
+
+
+  /*
+  you need to open the console and write
+  console.log(firstNumber)(secondNumber)(thirdNumber)(andSoOn)
+  and press enter
+  and you get as the result - sum of numbers, that were in brackets
+  */
