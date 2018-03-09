def0ca4ab1aee336271fd5420483ac5cd87795cf
Sophie Kirsanova <annie.apple.sun@gmail.com>
2018-03-09 22:27:15 +0300

    added the html to make the 'Battleship game' visible + added randomness to ship's position

--- OReilly/ex1-battleship.js
+++ OReilly/ex1-battleship.js
@@ -3,19 +3,20 @@ var isSunk = false;
 var guess;
 var guesses = 0;
 var hits = 0;
-var location1;
-var location2;
-var location3;
+var randomLocation = Math.floor(Math.random() * 5);
+var location1 = randomLocation;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 while (isSunk == false){
     guess = prompt("Ready, aim, fire! (enter a number from 0 to 6): ");
     if (guess < 0 || guess > 6){
         alert("Please enter a valid number!");
     } else {
-        guesses = guesses + 1;
+        guesses++;
         if (guess == location1 || guess == location2 || guess == location3){
             alert("HIT!");
-            hits = hits + 1;
+            hits++;
             if (hits == 3){
                 isSunk = true;
                 alert("You sank my battleship!");
@@ -28,3 +29,9 @@ while (isSunk == false){
 
 var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
 alert(stats);
+
+/*
+the problem here is:
+if you hit 3 times in one cell & if this cell is a part of the ship = program responds like you won (3 hits = sanking ship)
+that's kinda unacceptable
+*/
